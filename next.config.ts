927302a61463780ebb87733b891494f64796e542
Ray-wh <huzhuofan.hzf@foxmail.com>
2025-08-02 21:00:03 +0800

    add readme

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    // 开启相关实验性特性，可能与组件标记等功能相关
+    devtoolSegmentExplorer: true,
+  },
 };
 
 export default nextConfig;
--- src/app/first-post/page.tsx
+++ src/app/first-post/page.tsx
@@ -1,14 +1,12 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function FirstPost() {
   return (
     <>
       <h1>First Post</h1>
       <h2>
-        <Link href="/second-post">
-          Back to second post
-        </Link>
+        <Link href="/second-post">Back to second post</Link>
       </h2>
     </>
-  )
+  );
 }
